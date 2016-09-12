60f70b4ea94f72935f3aa5a13d0b04fcb89d9eb2
tkalra <tkalraoct31@gmail.com>
2016-09-12 14:23:39 -0700

    Added new and create actions

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,2 +1,19 @@
 class PicsController < ApplicationController
+
+def index 
+end 
+
+def new
+ @pic = Pic.new
+end 
+def create 
+ @pic = Pic.new(pic_params)
+end 
+
+private 
+ def pic_params
+	params.require(:pics).permit(:title, :description)
+	end
+
 end
+ 
